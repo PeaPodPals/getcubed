2e3c25b5c0e526535165041fdd4a773f548e5c32
StarrFox <StarrFox6312@gmail.com>
2023-06-05 23:07:11 +0000

    add reader

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,16 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var bios_dump_file = try std.fs.cwd().openFile(
+        "data/GameCube BIOS Images/gc-dvd-20010608.bin", .{});
+    defer bios_dump_file.close();
+
+    const reader = bios_dump_file.reader();
+    const data = reader.readAllAlloc(allocator, 10000000);
+    
+    std.log.info("read {any}", .{data});
 }
 
 test "basic test" {
